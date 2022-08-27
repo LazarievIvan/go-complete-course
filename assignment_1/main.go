f35af9152f3f489debea739cf652f24c4d8324eb
Lazariev Ivan <lazariev.ivan02@gmail.com>
2022-08-27 15:09:34 +0300

    GCC: assignment 1

---
+++ assignment_1/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var ints []int
+	for i := 0; i <= 10; i++ {
+		ints = append(ints, i)
+	}
+	for i := range ints {
+		if i%2 == 0 {
+			fmt.Printf("%d is even\n", i)
+		} else {
+			fmt.Printf("%d is odd\n", i)
+		}
+	}
+}
